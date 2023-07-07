85b3da78d513c30baa741aba877475ac0cb306a3
kuripasanda <ytkuripachanda@gmail.com>
2023-07-07 21:02:18 +0900

    [Add] VaultAPI

--- src/main/java/net/fukumaisaba/moneygive/MoneyGive.kt
+++ src/main/java/net/fukumaisaba/moneygive/MoneyGive.kt
@@ -3,16 +3,17 @@ package net.fukumaisaba.moneygive
 import dev.jorel.commandapi.CommandAPI
 import dev.jorel.commandapi.CommandAPIBukkitConfig
 import net.fukumaisaba.moneygive.util.DatabaseHelper
+import net.milkbowl.vault.economy.Economy
 import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin
 import kotlin.system.measureTimeMillis
+
 
 class MoneyGive : JavaPlugin() {
 
     companion object {
         lateinit var plugin: Plugin private set
-
-        private lateinit var dbHelper: DatabaseHelper
+        lateinit var vaultEconomy: Economy private set
     }
 
     override fun onEnable() {
@@ -22,7 +23,10 @@ class MoneyGive : JavaPlugin() {
         val time = measureTimeMillis {
 
             // データベース関連
-            dbHelper = DatabaseHelper()
+            val dbHelper = DatabaseHelper()
+
+            // VaultAPI 連携
+            setupEconomy()
 
             // CommandAPI 連携
             CommandAPI.onLoad(CommandAPIBukkitConfig(this))
@@ -36,6 +40,18 @@ class MoneyGive : JavaPlugin() {
 
     override fun onDisable() {
         // 停止処理
+    }
+
+
+    private fun setupEconomy(): Boolean {
+        if (server.pluginManager.getPlugin("Vault") == null) {
+            return false
+        }
+        val rsp = server.servicesManager.getRegistration(
+            Economy::class.java
+        ) ?: return false
+        vaultEconomy = rsp.provider
+        return true
     }
 
 }
